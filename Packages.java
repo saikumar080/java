43355f42356d52153d38e203266f1a2c738541df
saikumar pillalamarri <pillalamarrisaikumar8@gmail.com>
2024-09-24 05:29:05 +0530

    Packages

---
+++ Packages.java
@@ -0,0 +1,41 @@
+/**
+ * Packages:-In java it's used to group related classes.
+ * 2 types of packages:-
+ * 1.Built-in-Packages(packages from the Java API).
+ * 2.User-defined Packages(create ur on packages)
+ */
+
+
+ /**Built-in Packages
+  * Divided into packages and classes.
+  *classes :-u need to last mention with (.class)
+  *Packaages:-u need to last mention with (.*);
+  */
+
+  //----->>>> IMPORT a CLASS    <<<<------
+//   import java.util.Scanner;
+  //here java.util is the package,while Scanner is a class of the java.util package.
+
+
+  //--------->>>>>>>> Import a Packages    <<<<<<-------------
+    import java.util.*;
+
+    public class Packages {
+        public static void main(String [] args){
+            Scanner myobj=new Scanner(System.in);
+            System.out.println("Enter user name:: ");
+            String userName=myobj.nextLine();
+            System.out.println("Username is:: "+ userName);
+        }
+        
+    }
+
+
+    /**User-defined Packages:-
+     * |__root
+     *      |__mypack
+     *          |__MypackageClass.java
+     * more deatils in MyPackageClass.java file:::
+     */
+  
+
