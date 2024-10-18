6e5cb0a98ba430ab66d5d17fe80b60a9bce02f33
saikumar pillalamarri <pillalamarrisaikumar8@gmail.com>
2024-10-18 07:31:38 +0530

    Methods

--- Objects.java
+++ Objects.java
@@ -1,13 +1,13 @@
 public class Objects{
-    // int x=8,y;//class attribute.
-    // final int z=40;//Don't want the ability to override exisiting values
+    int x=8,y;//class attribute.
+    final int z=40;//Don't want the ability to override exisiting values
 
 
     //------>>>>>>>>    Examples for Attributes    <<<<<<---------
 
-    // String fname="john";
-    // String lname="Doe";
-    // int age=24;
+    String fname="john";
+    String lname="Doe";
+    int age=24;
 
 
 
@@ -16,15 +16,23 @@ public class Objects{
  * 1.Static:-Static methods can call without creating objects
  * 2.Public:-Public methods can be called by creating objects
  */
+
+ 
 //----->>>>>Static class Methods<<<<<<<---------
 static void  myStaticMethod(){
     System.out.println("Static methods can call without creating objects");
 }
-
+static void carOwnnedBy(String carName){
+    System.out.println("This car is owned by :: "+ carName);
+}
 
 //-------->>>>>>Public class Methods <<<<<<<-------
 public void myPublicMethod(){
     System.out.println("Public methods can be called by creating objects");
+}
+
+public void carNames(String Car){
+    System.out.println("this is my cars:: " + Car);
 }
 
 
@@ -42,32 +50,48 @@ public int carMaxSpeed(int speed){
     public static void main(String[] args) {
         
         // ------>>>>>>>>   creating an Object <<<<<---------
-        // Objects myObj= new Objects();
-        // System.out.println(myObj.x);//accessing attributes(.)
-        // System.out.println(myObj);
+        Objects myObj= new Objects();
+        System.out.println(myObj.x);//accessing attributes(.)
+        System.out.println(myObj);
+
+
 
         //--------->>>>>>>>>>> Creating a Multiple Objects   <<<<<<<----------
-        // Objects myobj1=new Objects();
-        // Objects myobj2=new Objects();
-        // myobj2.x=2;// overide the value
-        // System.out.println("Creating Mutliple Objects:::");
-        // System.out.println(myobj1.x);
-        // System.out.println(myobj2.x);
+        Objects myobj1=new Objects();
+        Objects myobj2=new Objects();
+        myobj2.x=2;// overide the value
+        System.out.println("Creating Mutliple Objects:::");
+        System.out.println(myobj1.x);
+        System.out.println(myobj2.x);
     
+
+
+
         //------->>>>>>>>   Modify Attributes    <<<<<<---------
-        // myObj.y=6;//modify;
-        // System.out.println(myObj.y);
-        // myObj.x=23;//overirde
-        // System.out.println(myObj.x);
+
+        System.out.println("Modifying the values of Class Attributes::  ");
+        myObj.y=6;//modify;
+        System.out.println("the value Y attributes from class:: "+ myObj.y);
+        myObj.x=23;//overirde
+        System.out.println("the value of X attributes from class:: "+myObj.x);
+
+
+
+
 
         //------>>>>>>> final Key Word     <<<<<<<-------
-        //System.out.println( "This is the keyword of final:: "+ myObj.z);
+        // 1.If you don't want the ability to override existing values:
+        // => declare the attribute as final:
+        System.out.println( "This is the keyword of final:: "+ myObj.z);
+
+
+
        
        // ------->>>>>>>>>>>>  Examples for Mutiple Attributes    <<<<<<<<<-----------
 
-        // Objects Details=new Objects();
-        // System.out.println("Name: " + Details.fname + " " + Details.lname);
-        // System.out.println("Age: " + Details.age);
+        Objects Details=new Objects();
+        System.out.println("Name: " + Details.fname + " " + Details.lname);
+        System.out.println("Age: " + Details.age);
 
 
 
@@ -75,14 +99,22 @@ public int carMaxSpeed(int speed){
 //------------------->>>>>>>>>>>>>>>          JAVA Class Methods            <<<<<<<<<<<<<<<----------------------------------------
         System.out.println("Java Class Methods::: ");
         myStaticMethod();
+        Objects.myStaticMethod();
         Objects publicMethod=new Objects();
+
         publicMethod.myPublicMethod();
+
         publicMethod.myStaticMethod();//we call static void class method also with objects.
+
+
+
         //----->>>>>>Accesss PublicMEthods with an Object <<<<<<<--------
         Objects myCar=new Objects();
         myCar.carColor();
         myCar.carModel("THAR 4*4");
         System.out.println("The MAximum speed of the car is :: "+myCar.carMaxSpeed(220));
+        myCar.carNames("Saikumar");
+        myCar.carOwnnedBy("Pillalamarri");
     }
 };
 
--- Second.java
+++ Second.java
@@ -1,12 +1,12 @@
 class Second{
     public static void main(String[] args){
         //---------->>>>>>>> This is code is excuting from Objects.java File
-        // Objects myObj=new Objects();
-        // System.out.println(myObj.x);
-        // Objects myCar=new Objects();
-        // myCar.carColor();
-        // myCar.carModel("Benz s-class");
-        // System.out.println("The Maximum speed of the car is :: "+220);
+        Objects myObj=new Objects();
+        System.out.println(myObj.x);
+        Objects myCar=new Objects();
+        myCar.carColor();
+        myCar.carModel("Benz s-class");
+        System.out.println("The Maximum speed of the car is :: "+220);
 
 
         //---------->>>>>>>>>>   This code is exiciting from the Modifiers.java File (Public Modifiers)  <<<<<<<<<<<<---------------
