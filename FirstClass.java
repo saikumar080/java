c9c4a01b8e60f7e07124aca7896d24c7020a23ba
saikumar pillalamarri <pillalamarrisaikumar8@gmail.com>
2024-10-27 07:05:34 +0530

    Switch cases

--- FirstClass.java
+++ FirstClass.java
@@ -35,14 +35,141 @@ class FirstClass{
 
 
         //-----------printing Tables----------------
-        System.out.println("Enter Number:: ");
-        int number=sc.nextInt();
-        int i=1;
-        while(i<=10){
-            int res=number * i;
-           System.out.println(number + " * " + i + " = " + res);
-           i++;
-        }
+        // System.out.println("Enter Number:: ");
+        // int number=sc.nextInt();
+        // int i=1;
+        // while(i<=10){
+        //     int res=number * i;
+        //    System.out.println(number + " * " + i + " = " + res);
+        //    i++;
+        // }
+
+
+//---------->>>>>>>>>>>>>>>if else statement<<<<<<<<<<--------------
+
+
+        //----Checking the candidate is adult or Not Adult--------------
+        // System.out.println("Enter age:: ");
+        // int age=sc.nextInt();
+        // if(age > 18){
+        //     System.out.println("Adult");
+        
+        // }else{
+        //     System.out.println("Not Adult");
+        // }
+
+
+
+        //----------Checking the given number is even or odd ---------------
+        // System.out.println("Enter the number:: ");
+        // int num=sc.nextInt();
+        // if(num % 2==0){
+        //     System.out.println("Even");
+        // }else{
+        //     System.out.println("Odd");
+        // }
+
+
+
+        //----comparing 2 numbers if both are equal print "Equal" if a>b print "a is Greater" if a<b print "a is lesser"-------------
+        // System.out.println("enter the value of A::");
+        // int a=sc.nextInt();
+        // System.out.println("Enter the value of b:: ");
+        // int b=sc.nextInt();
+        // if(a ==b){
+        //     System.out.println("Equals");
+        // }else if(a>b){
+        //     System.out.println("a is greater");
+        // }else{
+        //     System.out.println("a is lesser");
+        // }
+
+
+//---another way to write condtional without currly brackets-------
+        // if( a % 2==0)
+        //     System.out.println("Bazina");
+
+
+
+
+//-------->>>>>>>>>>>>> Switch Statements <<<<<<<<<<------------------
+        // System.out.println("Enter the Number::");
+        // int button=sc.nextInt();
+        // switch (button){
+        //     case 1: System.out.println("Hello");
+        //     break;
+        //     case 2: System.out.println("Namaste");
+        //     break;
+        //     case 3: System.out.println("Bonjour");
+        //     break;
+        //     default: System.out.println("Invalid");
+
+        // }
+
+
+
+
+        //----Take Home :::: Make a Calculator
+        // System.out.println("Enter a value:: ");
+        // int a= sc.nextInt();
+
+        // System.out.println("Enter the operator:: ");
+        // String operator=sc.next();
+
+        // System.out.println("Enter value of B:: ");
+        // int b=sc.nextInt();
+        
+        // System.out.println("The output value:: ");
+        // switch(operator){
+        //     case "+": System.out.println(a + b);
+        //     break;
+        //     case "-": System.out.println(a - b);
+        //     break;
+        //     case "*": System.out.println(a * b);
+        //     break;
+        //     case "/": System.out.println(a / b);
+        //     break;
+        //     case "%": System.out.println(a % b);
+        //     break;
+        //     default: System.out.println("Invalid operation");
+        // }
+
+
+
+
+        // //--Take Homae ask the user enter the number of month print month name::-----
+        // System.out.println("Enter Month Number:: ");
+        //     int month=sc.nextInt();
+        //     switch(month){
+        //         case 1: System.out.println("January");
+        //         break;
+        //         case 2: System.out.println("February");
+        //         break;
+        //         case 3: System.out.println("march");
+        //         break;
+        //         case 4: System.out.println("April");
+        //         break;
+        //         case 5: System.out.println("May");
+        //         break;
+        //         case 6: System.out.println("June");
+        //         break;
+        //         case 7: System.out.println("July");
+        //         break;
+        //         case 8: System.out.println("August");
+        //         break;
+        //         case 9: System.out.println("September");
+        //         break;
+        //         case 10: System.out.println("October");
+        //         break;
+        //         case 11: System.out.println("November");
+        //         break;
+        //         case 12: System.out.println("December");
+        //         break;
+        //         default:System.out.println(" Invalid Number");
+
+        //     }
+
+
         
     }
 }
